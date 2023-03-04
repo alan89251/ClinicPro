13a78e1a8425cc61b0be9da75affef581fae124e
Alan <csuen2@my.centennialcollege.ca>
2023-03-03 23:18:24 -0400

    Add navigation for patient detail and record

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:clinic_pro/components/modal/patient_detail_model.dart';
+import 'package:clinic_pro/components/modal/patient_records_model.dart';
 import 'package:clinic_pro/screens/login_screen.dart';
 import 'package:clinic_pro/screens/bottom_bar.dart';
+import 'package:clinic_pro/screens/patient_detail_screen.dart';
+import 'package:clinic_pro/screens/patient_records_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +22,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/patientDetail': (context) => ChangeNotifierProvider(
+          create: (context) => PatientDetailModel(),
+          child: const PatientDetailScreen()
+        ),
+        '/patientRecords': (context) => MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (context) => PatientDetailModel(),
+            ),
+            ChangeNotifierProvider(
+              create: (context) => PatientRecordsModel(),
+            ),
+          ],
+          child: const PatientRecordsScreen(),
+        ),
+      },
     );
   }
 }
